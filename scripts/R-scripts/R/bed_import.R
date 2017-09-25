9d9f51618302667e0d04c8e242b5848640f4c768
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2017-09-25 19:08:28 +0200

    Treat the case where a bed file does not contain a single (non-comment) line with tryCatch(), and in case of empty file return a data frame with 12 columns and 0 lines.

--- scripts/R-scripts/R/bed_import.R
+++ scripts/R-scripts/R/bed_import.R
@@ -15,17 +15,35 @@
 #' 
 #' @export
 BedImport <- function(path) {
-    # Gets the data frame from the file path.
-    regions <- as.data.frame(data.table::fread(path, 
-                                               header = FALSE,
-                                               sep = "\t", 
-                                               stringsAsFactors = FALSE, 
-                                               quote = ""))
-    # Renaming the data frame columns to fall in the bed standard.
-    columnNames <- c("chrom", "chromStart", "chromEnd", "name", "score", 
-                     "strand", "thickStart",
-                     "thickEnd", "itemRgb", "blockCount", "blockSizes",
-                     "blockStarts")
+  # # Get the data frame from the file path.
+  # regions <- as.data.frame(data.table::fread(path, 
+  #                                            header = FALSE,
+  #                                            comment.char="#",
+  #                                            sep = "\t", 
+  #                                            stringsAsFactors = FALSE, 
+  #                                            quote = ""))
+
+  # Get the data frame from the file path.
+  regions <- tryCatch(read.table(path, 
+                        header = FALSE,
+                        comment.char="#",
+                        sep = "\t", 
+                        quote = ""),  error=function(e) NULL)
+
+  # Renaming the data frame columns to fall in the bed standard.
+  columnNames <- c("chrom", "chromStart", "chromEnd", "name", "score", 
+                   "strand", "thickStart",
+                   "thickEnd", "itemRgb", "blockCount", "blockSizes",
+                   "blockStarts")
+  
+  
+  if (is.null(regions)) {
+    ## If bed file does not contain a single line, return a data.frame with the right number of columns but 0 lines
+    message("Warning: no line in bed file ", path)
+    regions <- as.data.frame(t(data.frame(row.names = columnNames)))
+  } else {
     colnames(regions) <- columnNames[1:ncol(regions)]
-    return(regions)
+  }
+  
+  return(regions)
 }
