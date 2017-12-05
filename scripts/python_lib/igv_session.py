288bd5e72b05ed1936357bfa6b535f715bfb8811
Claire Rioualen <Claire.Rioualen@inserm.fr>
2017-12-05 18:17:24 +0100

    updating IGV session

--- scripts/python_lib/igv_session.py
+++ scripts/python_lib/igv_session.py
@@ -27,12 +27,13 @@ import os
 
 
 filename = snakemake.output[0]
-genome = snakemake.params["genome_fasta"]
-gff3 = snakemake.params["genome_gff3"]
-gtf = snakemake.params["genome_gtf"]
+genome = snakemake.input["genome_fasta"]
+gff3 = snakemake.input["genome_gff3"]
+gtf = snakemake.input["genome_gtf"]
+peaks = snakemake.input["peaks"]
 
 if __name__ == '__main__':
-    igv_session(filename, genome, gff3, gtf)
+    igv_session(filename, genome, gff3, gtf, peaks)
 
 
 def igv_session(filename, genome, gtf="", gff3="", peaks="", coverage="", transcripts_gtf=""):
@@ -110,10 +111,5 @@ def igv_session(filename, genome, gtf="", gff3="", peaks="", coverage="", transc
     file.write('</Session>\n')
 
     file.close()
-
-
-if __name__ == '__main__':
-    igv_session(filename, genome, gff3, gtf)
-
 
 
