413f20d9142404cb3382734c778e253b3e849154
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-07-25 09:38:06 +0200

    moved function from deg_lib.R to separate script and renamed   R/calcstatsPerSample.R R/calc_stats_per_sample.R

--- scripts/RSnakeChunks/R/deg_lib.R
+++ scripts/RSnakeChunks/R/deg_lib.R
@@ -85,45 +85,6 @@ verbose <- function(message.content,
 #'
 #' @details
 
-
-
-## Compute sample-wise statistics
-calc.stats.per.sample <- function(sample.descriptions,
-                                  count.table) {
-  verbose("Computing statistics per sample", 2)
-
-  stats.per.sample <- cbind(
-    sample.desc[names(count.table), ],
-    data.frame(
-      "zeros" = apply(count.table == 0, 2, sum, na.rm = TRUE), ## Number of genes with 0 counts
-      "detected" = apply(count.table > 0, 2, sum, na.rm = TRUE), ## Number of genes counted at least once
-      "sum" = apply(count.table, 2, sum, na.rm = TRUE), ## Sum of all counts for the sample
-      "mean" = apply(count.table, 2, mean, na.rm = TRUE), ## Mean counts per gene
-      "min" = apply(count.table, 2, min, na.rm = TRUE), ## Min counts per gene
-      "perc05" = apply(count.table, 2, quantile, probs = 0.05, na.rm = TRUE), ## 5th percentile
-      "perc25" = apply(count.table, 2, quantile, probs = 0.25, na.rm = TRUE), ## 25th percentile
-      "median" = apply(count.table, 2, median, na.rm = TRUE), ## median (percentile 50)
-      "perc75" = apply(count.table, 2, quantile, probs = 0.75, na.rm = TRUE), ## percentile 75
-      "perc90" = apply(count.table, 2, quantile, probs = 0.90, na.rm = TRUE), ## percentile 95
-      "perc95" = apply(count.table, 2, quantile, probs = 0.95, na.rm = TRUE), ## percentile 95
-      "max" = apply(count.table, 2, max, na.rm = TRUE) ## Max counts per gene
-    )
-  )
-  rownames(stats.per.sample) <- colnames(all.counts)
-  stats.per.sample$max.sum.ratio <- stats.per.sample$max / stats.per.sample$sum
-  stats.per.sample$median.mean.ratio <- stats.per.sample$media / stats.per.sample$mean
-  stats.per.sample$Mreads <- round(stats.per.sample$sum/1e6, digits = 1)
-
-  ## Count number and the fraction of samples with counts below the mean.
-  ## This shows the impact of very large counts: in some samples,
-  ## 85% of the samples have a value below the mean (i.e. the mean is at the percentile 85 !)
-  stats.per.sample$below.mean <- apply(t(count.table) < stats.per.sample$mean, 1, sum, na.rm = TRUE)
-  stats.per.sample$fract.below.mean <- stats.per.sample$below.mean/nrow(count.table)
-  # View(stats.per.sample)
-
-  return(stats.per.sample)
-}
-
 #' @title Draw a barplot with the number of reads per sample
 #' @author Jacques van Helden
 #' @param stats.per.sample a table with the sample-wise statistics, which ca n be produced by RowStats().
