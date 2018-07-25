810b93c9ecfbe19918bb752a465fcc9f24401c23
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-07-25 09:32:30 +0200

    moved piece of code (empty function, to be implemented) from deg_lib to a separate file

--- scripts/RSnakeChunks/R/deg_lib.R
+++ scripts/RSnakeChunks/R/deg_lib.R
@@ -84,30 +84,7 @@ verbose <- function(message.content,
 #' and the volcano plots commonly used to show the result of a t-test with microarrays.
 #'
 #' @details
-#' First version: 2015-09
-#' Last modification: 2015-09
-#'
-#' @param deg.table A table with the results of differential expression. Such tables can be produced by DESeq2, edgeR or other packages.
-#' @param alpha=0.05 Threshold to declare features (genes) as positive or negative.
-#' @param effect.size.col="log2FC" Number or name of the column containing the effect size, whose values will be displayed on the X axis.
-#' @param control.type = "p.value" Control type. Can be a p-value or an equivalent estimation of significance (FDR, e-value, FWER, ...). This value will be converted to y=-log10(x) to obtain the values displayed on the Y axis. The alpha threshold applies on the non-log-converted values.
-#' @param ...
-#'
-#' @examples
-#'
-#' @export
-rnaseq.volcanoPlot <- function(deg.table,
-                               alpha = 0.05,
-                               effect.size.col = "log2FC",
-                               control.type = "p.value",
-                               ...) {
-  stop("NOT IMPLEMENTED YET, ASK Jacques.van-Helden@univ-amu.fr")
-  VolcanoPlot(deg.table,
-              alpha = 0.05,
-              effect.size.col = "log2FC",
-              control.type = "p.value",
-              ...)
-}
+
 
 
 ## Compute sample-wise statistics
