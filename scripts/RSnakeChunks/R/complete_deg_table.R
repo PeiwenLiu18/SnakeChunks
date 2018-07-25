f39cec09b28e6ee51c55e907aac37eb0ecfd7081
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-07-25 09:26:39 +0200

    moved function from deg_lib.R to separate script and renamed  R/completeDegTable.R R/complete_deg_table.R

--- scripts/RSnakeChunks/R/complete_deg_table.R
+++ scripts/RSnakeChunks/R/complete_deg_table.R
@@ -1,3 +1,4 @@
+
 #' @title Analyse a table of differentially expressed genes.
 #'
 #' @author Jacques van Helden (\email{Jacques.van-Helden@@univ-amu.fr})
@@ -31,6 +32,7 @@
 #' output table. Set to NA to avoid rounding.
 #'
 #' @param dir.figures=NULL if not NULL, figures will be saved in the specified directory.
+#' @param verbose=0 level of verbosity
 #'
 #' @examples
 #'
@@ -41,18 +43,21 @@
 #'     "pvalue" = deseq2.res$pvalue,
 #'     "padj" = deseq2.res$padj)
 #' deseq2.result.table <- complete.deg.table(deseq2.result.table,
-#'     table.name="DESeq2", sort.column="padj")
+#'     table.name="DESeq2", sort.column = "padj")
 #'
 #' @export
-completeDegTtable <- function(deg.table,
+complete.deg.table <- function(deg.table,
                                table.name,
                                sort.column = "none",
-                               thresholds=thresholds,
+                               thresholds = c(),
                                round.digits = 3,
-                               dir.figures=NULL) {
+                               dir.figures = NULL,
+                               verbose = 0) {
 
   # names(deg.table)
-  verbose(paste("Analysing", table.name, "result table with", nrow(deg.table), "rows"), 1)
+  if (verbose >= 1) {
+    message("\tcomplete.deg.table()\tTable name: ", table.name, "\t", nrow(deg.table), " features (rows)")
+  }
 
   col.descriptions <- vector() ## Initialize vector with column descriptions
 
@@ -68,10 +73,10 @@ completeDegTtable <- function(deg.table,
   row.names(deg.table) <- deg.table$gene.id
 
   ## Sort DEG table if required
-  sort.decreasing <- c("mean"=TRUE,
-                       "padj"=FALSE,
-                       "pvalue"=FALSE,
-                       "log2FC"=TRUE)
+  sort.decreasing <- c("mean" = TRUE,
+                       "padj" = FALSE,
+                       "pvalue" = FALSE,
+                       "log2FC" = TRUE)
   if (sort.column != "none") {
     verbose(paste("\tSorting DEG table by", sort.column), 2)
     deg.table <- deg.table[order(deg.table[,sort.column],
@@ -102,21 +107,37 @@ completeDegTtable <- function(deg.table,
   col.descriptions["sign"] <- "Sign of the regulation (1= up, -1 = down)"
 
   ## Label the genes passing the FDR, E-value and fold-change thresholds
-  threshold.type <- c("pvalue"="upper", "padj"="upper", "evalue"="upper", "FC"="lower")
-  selection.columns <- paste(sep="", names(thresholds), "_", thresholds)
-  names(selection.columns) <- names(thresholds)
-  for (s in names(thresholds)) {
+  threshold.type <- c(
+    "pvalue" = "upper",
+    "padj" = "upper",
+    "evalue" = "upper",
+    "FC" = "lower")
+  thresholds.to.apply <- intersect(names(thresholds), names(threshold.type))
+  if (verbose >= 1) {
+    message("\t\tApplying thresholds: ", paste(collapse = ", ", thresholds.to.apply))
+  }
+  selection.columns <- paste(sep = "", thresholds.to.apply, "_", thresholds[thresholds.to.apply])
+  names(selection.columns) <- thresholds.to.apply
+  for (s in thresholds.to.apply) {
     if (threshold.type[s] == "upper") {
       selected <- deg.table[, s] < thresholds[s]
     } else {
       selected <- deg.table[, s] > thresholds[s]
     }
+    selected[is.na(selected)] <- FALSE
+    #table(selected)
     deg.table[, selection.columns[s]] <- selected*1
     col.descriptions[selection.columns[s]] <- paste("Passing", threshold.type[s], "threshold on", s)
+    message("\t\tApplied ", threshold.type[s],
+            " threshold on ", s, "\t", thresholds[s],
+            "; remaining features: ",  sum(selected))
   }
   ## Select genes passing all thresholds
+  if (verbose  >= 2) {
+    message("\t\tSelection columns: ", paste(collapse = ", ", selection.columns))
+  }
   deg.table[,"DEG"] <-
-    1*(apply(deg.table[,selection.columns],1,sum) == length(thresholds))
+    1*(apply(deg.table[,selection.columns], 1, sum) == length(thresholds.to.apply))
 
   # print(data.frame(col.descriptions))
 
@@ -128,12 +149,11 @@ completeDegTtable <- function(deg.table,
     for (col in setdiff(names(deg.table), selection.columns)) {
       if (is.numeric(deg.table[,col])) {
         verbose(paste("Rounding", col), 2)
-        deg.table[,col] <- signif(digits=3, deg.table[,col])
+        deg.table[,col] <- signif(digits = 3, deg.table[,col])
       }
     }
   }
 
-  ################################################################
   ## Export figures
   if (!is.null(dir.figures)) {
     verbose(paste("\tSaving figures in directory", dir.figures), 2)
@@ -142,17 +162,17 @@ completeDegTtable <- function(deg.table,
     ## Draw Venn diagram with number of genes declared significant
     ## according to the selection criteria (threshold fields).
     selection.venn.counts <- vennCounts(deg.table[,selection.columns])
-    pdf(file=file.path(dir.figures, paste(sep = "", table.name, "selection_Venn.pdf")))
-    vennDiagram(selection.venn.counts, cex=1, main=paste(table.name, "selected genes"))
+    pdf(file = file.path(dir.figures, paste(sep = "", table.name, "selection_Venn.pdf")))
+    vennDiagram(selection.venn.counts, cex = 1, main = paste(table.name, "selected genes"))
     silence <- dev.off()
 
     ## Histogram of the nominal p-values
-    pdf(file=file.path(dir.figures, paste(sep = "", table.name, "_pval_hist.pdf")), width=7, height=5)
-    hist(deg.table$pvalue, breaks=seq(from=0, to=1, by=0.05),
-         xlab="Nominal p-value",
-         ylab="Number of genes",
-         main=paste(table.name, "pvalue distribution"),
-         col="#BBBBBB")
+    pdf(file = file.path(dir.figures, paste(sep = "", table.name, "_pval_hist.pdf")), width = 7, height = 5)
+    hist(deg.table$pvalue, breaks = seq(from = 0, to = 1, by = 0.05),
+         xlab = "Nominal p-value",
+         ylab = "Number of genes",
+         main = paste(table.name, "pvalue distribution"),
+         col = "#BBBBBB")
     silence <- dev.off()
   }
   return(deg.table)
