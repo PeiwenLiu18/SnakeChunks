69e2c3787c5ec66d678a743cd2d3ed52107aacb1
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-07-27 05:48:43 +0200

    fixed some dependencies

--- scripts/RSnakeChunks/R/margin_stats.R
+++ scripts/RSnakeChunks/R/margin_stats.R
@@ -32,6 +32,7 @@
 #'  \item{mean.median.ratio}{ratio between mean and the median, which can be interpreted as skew indicator. }
 #'  \item{below.mean}{number of values below the mean. }
 #'  \item{fract.below.mean}{fraction of values below the mean. }
+#'  \item{Mcounts}{Million counts = round(digits=2, sum/1e6). Classically used for Next-Generation Sequencing libraries.  }
 #' }
 #' @export
 MarginStats <- function(x,
@@ -74,7 +75,8 @@ MarginStats <- function(x,
                        "max.sum.ratio",
                        "mean.median.ratio",
                        "below.mean",
-                       "fract.below.mean"
+                       "fract.below.mean",
+                       "Mcounts"
   )
   if (is.null(selected.stats)) {
     selected.stats <- supported.stats
@@ -96,91 +98,124 @@ MarginStats <- function(x,
 
 
   ## Compute margin statistics
+  if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise mean") }
   margin.stats <- data.frame(
     mean = apply(x, margin, mean, na.rm = TRUE)
   )
   if ("median" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise median") }
     margin.stats$median = apply(x, margin, quantile, na.rm = TRUE, probs = 0.5)
   }
-  if ("sum" %in% selected.stats) {
+  if (length(intersect(selected.stats, c("sum", "Mcounts", "max.sum.ratio"))) > 0) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise sum") }
     margin.stats$sum = apply(x, margin, sum, na.rm = TRUE)
   }
   if ("sd" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise sd") }
     margin.stats$sd = apply(x, margin, sd, na.rm = TRUE)
   }
   if ("iqr" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise iqr") }
     margin.stats$iqr = apply(x, margin, IQR, na.rm = TRUE)
   }
   if ("var" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise var") }
     margin.stats$var = apply(x, margin, var, na.rm = TRUE)
   }
   if ("min" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise min") }
     margin.stats$min = apply(x, margin, min, na.rm = TRUE)
   }
   if ("max" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise max") }
     margin.stats$max = apply(x, margin, max, na.rm = TRUE)
   }
   if ("perc01" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise perc01") }
     margin.stats$perc01 = apply(x, margin, quantile, na.rm = TRUE, probs = 0.01)
   }
   if ("perc05" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise perc05") }
     margin.stats$perc05 = apply(x, margin, quantile, na.rm = TRUE, probs = 0.05)
   }
   if ("perc10" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise perc10") }
     margin.stats$perc10 = apply(x, margin, quantile, na.rm = TRUE, probs = 0.10)
   }
   if ("Q1" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise Q1") }
     margin.stats$Q1 = apply(x, margin, quantile, na.rm = TRUE, probs = 0.25)
   }
   if ("Q3" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise Q3") }
     margin.stats$Q3 = apply(x, margin, quantile, na.rm = TRUE, probs = 0.75)
   }
   if ("perc90" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise perc90") }
     margin.stats$perc90 = apply(x, margin, quantile, na.rm = TRUE, probs = 0.90)
   }
   if ("perc95" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise perc95") }
     margin.stats$perc95 = apply(x, margin, quantile, na.rm = TRUE, probs = 0.95)
   }
   if ("perc99" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise perc99") }
     margin.stats$perc99 = apply(x, margin, quantile, na.rm = TRUE, probs = 0.90)
   }
   if ("zeros" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise zeros") }
     margin.stats$zeros = apply(x == 0, margin, sum, na.rm = TRUE)
   }
   if ("na.values" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise na.values") }
     margin.stats$na.values = apply(is.na(x), margin, sum)
   }
   if ("infinite.values" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise infinite.values") }
     margin.stats$infinite.values = apply(is.infinite(as.matrix(x)), margin, sum)
   }
   if ("non.null" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise non.null") }
     margin.stats$non.null = apply(x > 0, margin, sum, na.rm = TRUE)
   }
   if ("sdiqr" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise sdiqr") }
     margin.stats$sdiqr = margin.stats$iqr/(2*qnorm(3/4))
   }
   if ("max.mean.ratio" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise max.mean.ratio") }
     margin.stats$max.mean.ratio <- margin.stats$max/margin.stats$mean
   }
   if ("max.sum.ratio" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise max.sum.ratio") }
     margin.stats$max.sum.ratio <- margin.stats$max/margin.stats$sum
   }
   if ("mean.median.ratio" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise mean.median.ratio") }
     margin.stats$mean.median.ratio <- margin.stats$mean / margin.stats$median
   }
-  if ("below.mean" %in% selected.stats) {
+  if (length(intersect(c("below.mean", "frac.below.mean"), selected.stats)) > 0) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise below.mean") }
     margin.stats$below.mean <- apply(t(x) < margin.stats$mean, 1, sum, na.rm = TRUE)
   }
   if ("fract.below.mean" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise fract.below.mean") }
     margin.stats$fract.below.mean <- margin.stats$below.mean/nrow(x)
   }
-
-  ## Assign same names as in data table
-  if (margin == 1) {
-    rownames(margin.stats) = rownames(x)
-  } else if (margin == 2) {
-    colnames(margin.stats) = colnames(x)
+  if ("Mcounts" %in% selected.stats) {
+    if (verbose >= 2) { message("\t\tComputing ", margin.name, "-wise Mcounts") }
+    margin.stats$Mcounts <- round(digits = 3, margin.stats$sum/1e6)
   }
+
+  # ## Assign same names as in data table
+  # if (margin == 2) {
+  #   rownames(margin.stats) = rownames(x)
+  # } else if (margin == 1) {
+  #   colnames(margin.stats) = colnames(x)
+  # }
+
+  ## Return selected stats in the specified order
+  margin.stats <- margin.stats[, selected.stats]
 
   return(margin.stats)
 }
@@ -204,7 +239,7 @@ RowStats <- function(x, verbose = 0) {
 #' @description statistics are computed on each column by passing the data frame/matrux to MarginStats() with marrgin=2.
 #' @export
 ColStats <- function(x, verbose = 0, selected.stats = NULL) {
-  MarginStats(x, 2, verbose = 0)
+  MarginStats(x, 2, verbose, selected.stats)
 }
 
 
@@ -233,8 +268,11 @@ ColStats <- function(x, verbose = 0, selected.stats = NULL) {
 #' stats.per.row <- RowStats(denboer2009.expr, verbose = 1)
 #' head(stats.per.row)
 #' @export
-statsPerRow <- function(x, ...) {
+statsPerRow <- function(x,
+                        margin,
+                        verbose = 0,
+                        selected.stats = NULL) {
   message("statsPerRow() has been replaced by RowStats()")
-  return(RowStats(x, ...))
+  return(RowStats(x, 1, verbose, selected.stats))
 }
 
