52589ea8ce6c9233b1b7a16f9fd860a0ee3e12f5
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-08-17 06:46:11 +0200

    fixed bug occurring with // in the paths

--- scripts/RSnakeChunks/R/relative_path.R
+++ scripts/RSnakeChunks/R/relative_path.R
@@ -4,51 +4,55 @@
 #' @param target target, i.e. the file that will be pointed by the relative path
 #' @param sourceIsDir=FALSE Boolean indicating whether the source is a file or a directory
 #' @examples
-#' 
+#'
 #' ## From file to file with shared path
 #' RelativePath(
-#'   source = "RNA-seq/results/diffexpr/reports/myreport.Rmd", 
+#'   source = "RNA-seq/results/diffexpr/reports/myreport.Rmd",
 #'   target = "RNA-seq/results/diffexpr/a_vs_b/DEG/deg_table.tsv",
 #'   sourceIsDir = FALSE)
 #'
 #' ## From file to file without shared path
 #' RelativePath(
-#'   source = "RNA-seq/results/diffexpr/reports/myreport.Rmd", 
+#'   source = "RNA-seq/results/diffexpr/reports/myreport.Rmd",
 #'   target = "metadata/sample_descriptions.tsv",
 #'   sourceIsDir = FALSE)
 #'
 #' ## From file to file with the source in current dir
 #' RelativePath(
-#'   source = "results/myreport.Rmd", 
+#'   source = "results/myreport.Rmd",
 #'   target = "results/figures/myfigure.png",
 #'   sourceIsDir = FALSE)
-#'   
+#'
 #' ## From file to dir
 #' RelativePath(
-#'   source = "RNA-seq/results/diffexpr/reports/myreport.Rmd", 
+#'   source = "RNA-seq/results/diffexpr/reports/myreport.Rmd",
 #'   target = "RNA-seq/results/samples/",
 #'   sourceIsDir = FALSE)
-#'   
+#'
 #' ## From dir to dir: requires to specify sourceIsDIR=TRUE
 #' RelativePath(
-#'   source = "RNA-seq/results/diffexpr/reports", 
+#'   source = "RNA-seq/results/diffexpr/reports",
 #'   target = "RNA-seq/results/samples",
 #'   sourceIsDir = TRUE)
-#'   
+#'
 #' ## From file to upper dir
 #' RelativePath(
-#'   source = "RNA-seq/results/diffexpr/reports/myreport.Rmd", 
+#'   source = "RNA-seq/results/diffexpr/reports/myreport.Rmd",
 #'   target = "RNA-seq/results/",
 #'   sourceIsDir = FALSE)
-#'   
+#'
 #' ## From file to its own dir
 #' RelativePath(
-#'   source = "RNA-seq/results/diffexpr/reports/myreport.Rmd", 
+#'   source = "RNA-seq/results/diffexpr/reports/myreport.Rmd",
 #'   target = "RNA-seq/results/diffexpr/reports/",
 #'   sourceIsDir = FALSE)
-#'   
+#'
 #' @export
 RelativePath <- function(source, target, sourceIsDir = FALSE, verbose = 0) {
+
+  ## Source path
+  source <- gsub(pattern = "/+", replacement = "/", x = source) ## Remove double /
+
   if (sourceIsDir) {
     sourcePath <- unlist(strsplit(x = source, split = "/"))
   } else {
@@ -57,8 +61,11 @@ RelativePath <- function(source, target, sourceIsDir = FALSE, verbose = 0) {
   if ((length(sourcePath) == 1) && (sourcePath == ".")) {
     sourcePath <- vector() ## replace dot by empty vector
   }
+
+  ## Target path
+  target <- gsub(pattern = "/+", replacement = "/", x = target) ## Remove double /
   targetPath <- unlist(strsplit(x = target, split = "/"))
-  
+
   ## Compute match bewteen source and target paths
   pathMatch <- match(sourcePath, targetPath)
   if ((length(pathMatch) == 0) || is.na(pathMatch)) {
@@ -66,21 +73,21 @@ RelativePath <- function(source, target, sourceIsDir = FALSE, verbose = 0) {
   } else {
     common.depth <- max(pathMatch, na.rm = TRUE)
   }
-  
+
   ## Define back path as the path from the source to the shared path root
   if (length(sourcePath) > common.depth) {
     backPath <- paste(collapse = "/", rep(x = "..", length.out = length(sourcePath) - common.depth))
   } else {
     backPath <- vector() ## empty vector
   }
-  
+
   ## Define forward path path from the shared path root to the target
   if (length(targetPath) > common.depth) {
     fwdPath <- paste(collapse = "/", targetPath[(common.depth + 1):length(targetPath)])
   } else {
     fwdPath <- vector() ## empty vector
   }
-  
+
   ## Combine back and forward path
   if ((length(backPath) == 0) && (length(fwdPath) == 0)) {
     relPath = "."
@@ -101,6 +108,6 @@ RelativePath <- function(source, target, sourceIsDir = FALSE, verbose = 0) {
     message("\t\tfwdPath\t", fwdPath)
     message("\t\trelPath\t", relPath)
   }
-  
+
   return(relPath)
 }
