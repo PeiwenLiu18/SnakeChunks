bcbff0d7075c03d220fa53b48796b5dc160cef2e
Claire Rioualen <rioualenclaire@gmail.com>
2019-06-07 15:53:49 -0500

    corrected inconsistency in params names

--- scripts/RSnakeChunks/misc/mosaics.R
+++ scripts/RSnakeChunks/misc/mosaics.R
@@ -5,14 +5,14 @@ input.treatment <- snakemake@input[["treatment"]]
 input.control <- snakemake@input[["control"]]
 
 
-output.peaks_broadPeak <- snakemake@output[["peaks_broadPeak"]]
-output.peaks_narrowPeak <- snakemake@output[["peaks_narrowPeak"]]
+#output.peaks_broadPeak <- snakemake@output[["peaks_broadPeak"]]
+output.peaks <- snakemake@output[["peaks"]]
 output.peaks_bed <- snakemake@output[["peaks_bed"]]
 
 params.outdir <- snakemake@params[["outdir"]]
-params.fdr <- snakemake@params[["fdr"]]
-params.frag_len = snakemake@params[["frag_len"]]
-params.bin_size = snakemake@params[["bin_size"]]
+params.FDR <- as.numeric(snakemake@params[["FDR"]])
+params.frag_len = as.numeric(snakemake@params[["frag_len"]])
+params.bin_size = as.numeric(snakemake@params[["bin_size"]])
 params.bin_treatment = snakemake@params[["bin_treatment"]]
 params.bin_control = snakemake@params[["bin_control"]]
 
@@ -28,7 +28,7 @@ constructBins(  input.control,  fileFormat="bed", outfileLoc=params.outdir,
 fileName <- c(params.bin_treatment, params.bin_control)
 
 bin <- readBins( type=c("chip","input"), fileName=fileName )
-fit <- mosaicsFit( bin, analysisType="IO", bgEst="automatic" )
+fit <- mosaicsFit( bin, analysisType="IO", bgEst="rMOM", truncProb = 0.8)
 peak <- mosaicsPeak( fit, signalModel="2S", FDR={params.FDR}, maxgap=400, minsize=36, thres=10 )  ###
 peak <- extractReads( peak,
     input.treatment, chipFileFormat="bed", chipPET=FALSE, chipFragLen={params.frag_len},
